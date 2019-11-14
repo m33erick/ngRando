d08db57184ba6cc2a2a283ce225e3516ed9eee8e
m33erick <m33erick@gmail.com>
2019-11-14 18:52:19 +0100

    routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HikeModule } from './hike/hike.module';
+import { HomeMudule } from './home/home.module';
+import { HomeComponent } from './home/home.component';
+import { HikeListeComponent } from './hike/hike-list.component';
+import { PageNotFoundComponent } from './page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PageNotFoundComponent
   ],
   imports: [
-    BrowserModule, HikeModule, HttpClientModule
+    BrowserModule, HikeModule, HttpClientModule, HomeMudule,
+    RouterModule.forRoot([
+      {path: 'homme', component: HomeComponent },
+      {path: 'hike', component: HikeListeComponent },
+      {path: '', redirectTo: 'home', pathMatch: 'full' },
+      {path: '**', component: PageNotFoundComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page-not-found.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+    template: 'Page non trouvée'
+})
+export class PageNotFoundComponent { }
